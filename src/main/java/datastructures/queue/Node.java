2b0261cbb525f1fa82e24a0deb110893382b285f
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-10-12 12:00:26 +0300

    Added Queue collection

---
+++ src/main/java/datastructures/queue/Node.java
@@ -0,0 +1,10 @@
+package datastructures.queue;
+
+public class Node {
+    Object value;
+    Node next;
+
+    public Node(Object value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/datastructures/queue/Queue.java
@@ -0,0 +1,13 @@
+package datastructures.queue;
+
+public interface Queue {
+    void enqueue(Object value);
+
+    Object dequeue();
+
+    Object peek();
+
+    int size();
+
+
+}
